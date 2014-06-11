3e3f273634e8cb4b8dce83ff0d6ef6465e378a3b
Bryan Wu <bryan.wu@bryonics.com>
2014-06-11 21:52:16 +1000

    Fixing up frontpage button links

--- var/cache/templates/customer/%%38^389^389EB893%%frontpage_cta_buttons.tpl.php
+++ var/cache/templates/customer/%%38^389^389EB893%%frontpage_cta_buttons.tpl.php
@@ -1,13 +1,13 @@
-<?php /* Smarty version 2.6.18, created on 2014-06-11 13:06:49
+<?php /* Smarty version 2.6.18, created on 2014-06-11 21:36:54
          compiled from C:/wamp5/www/public_html/skins/basic/customer/addons/billibuys/blocks/static_templates/frontpage_cta_buttons.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('modifier', 'fn_url', 'C:/wamp5/www/public_html/skins/basic/customer/addons/billibuys/blocks/static_templates/frontpage_cta_buttons.tpl', 1, false),)), $this); ?>
-<?php  ob_start();  ?><a href="<?php echo fn_url('auth.login_form'); ?>
+<?php  ob_start();  ?><a href="<?php echo fn_url('billibuys.view'); ?>
 "><span class="flat_link grey try-us">
 	Try us out
 </span></a>
 
-<a href="<?php echo fn_url('pages.view&page_id=30'); ?>
+<a href="<?php echo fn_url(''); ?>
 "><span class="flat_link red learn-more">
 	Learn more
 </span></a>
