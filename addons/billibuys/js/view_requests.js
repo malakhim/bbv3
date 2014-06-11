d831f4aa52238c8ca72cba2c91fbc334b8293639
Bryan Wu <bryan.wu@bryonics.com>
2014-06-11 20:52:08 +1000

    Uploading timer fixes

--- addons/billibuys/js/view_requests.js
+++ addons/billibuys/js/view_requests.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	// console.log(expiry_date.getHours());
 	$('.bb-list-time-remaining').each(function(){
 		var date = new Date($(this).attr('expiry') * 1000);
-		var expiry_date = new Date(date.getFullYear(), date.getMonth()- 1, date.getDate());
+		var expiry_date = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
 		console.log(expiry_date.getFullYear() + " " + expiry_date.getMonth() + " " + expiry_date.getDate());
 		$(this).countdown({until: expiry_date});
 	});
