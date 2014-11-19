8aff46e356fb1d13733f0b5dfba0df07cb761d6b
malakhim <bryan@bryonics.com>
2014-11-19 14:49:27 +1100

    Fix for navbar padding-bottom

--- var/cache/templates/customer/%%22^22D^22D9C69F%%billibuys_top_nav_login.tpl.php
+++ var/cache/templates/customer/%%22^22D^22D9C69F%%billibuys_top_nav_login.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.18, created on 2014-11-19 13:58:58
+<?php /* Smarty version 2.6.18, created on 2014-11-19 14:49:05
          compiled from C:/wamp3/www/public_html/skins/basic/customer/addons/billibuys/blocks/static_templates/billibuys_top_nav_login.tpl */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('modifier', 'fn_url', 'C:/wamp3/www/public_html/skins/basic/customer/addons/billibuys/blocks/static_templates/billibuys_top_nav_login.tpl', 4, false),)), $this); ?>
--- var/cache/templates/customer/%%9B^9B0^9B07315D%%features_panel_2.tpl.php
+++ var/cache/templates/customer/%%9B^9B0^9B07315D%%features_panel_2.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.18, created on 2014-11-17 22:32:16
+<?php /* Smarty version 2.6.18, created on 2014-11-19 14:49:06
          compiled from C:/wamp3/www/public_html/skins/basic/customer/addons/billibuys/blocks/static_templates/features_panel_2.tpl */ ?>
 <?php  ob_start();  ?><div class="fpanel-title">Get more value</div>
 <br/>
